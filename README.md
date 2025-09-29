d35cf20fb519f8763eeb2f14e183aaca4c3db68a
x86root <191064573+x86root@users.noreply.github.com>
2025-09-28 22:10:49 -0400

    Large feature overhaul
    Implemented a tab system to allow multiple docs to be open at once. This required a new function to get the editor object for other events like undo, redo, cut and paste, etc. Plan to implement more key shortcuts using these.
    
    I learned lambda functions somewhat with having to define my first one as the getEditor function and using it on the event of those previously listed. I plan to implement these more as the project goes on.
    
    Also added a stylesheet which will allow me to further customize the layout/GUI.

--- README.md
+++
@@ -1,2 +0,0 @@
-# Multi-Language-IDE-v1.0
-A Python/PyQt5 based IDE for programmers and developers offering one platform for compiling and running different languages. As a dev, a tedious task can be having to switch IDE's for different languages. While VS Code offers a similar solution,  it is a very heavy program with  unneccesary files. My goal is to make an IDE that is minimal &amp; simple.
--- multi_lang_ide.py
+++ multi_lang_ide.py
@@ -16,35 +16,45 @@ class MainWindow(QMainWindow):
         super(MainWindow, self).__init__()
 
         # Sets window size (x, y, width, height), temporarily smaller
-        self.setGeometry(100, 100, 800, 600)
+        self.setGeometry(100, 100, 1800, 1000)
+        
+        self.setWindowTitle("CodeIDE")
 
-        # Sets vertical layout for GUI, makes the text editor center/ top-to-bottom
-        layout = QVBoxLayout()
+        # Sets tab layout for GUI, allows for multiple documents to be open at once
         
-        # Building editor object using QPlainTextEdit()
-        self.editor = QPlainTextEdit()
-        
+        self.tabs = QTabWidget()
+
         # self.path holds the path of the currently open file.
         # If none, there is currently no file open or it is a new file.
         self.path = None
         
-        # Then adds the new editor to the layout as a widget
-        layout.addWidget(self.editor)
+        # Adds these after to fit the layout: file label, text editor, console
+        #layout.addWidget(self.editor)
+        #layout.addWidget(self.output_console)
 
-        # Container widget that holds everything
-        container = QWidget()
         
-        # Sets layout of container as our previous layout including widgets
-        container.setLayout(layout)
-        
-        # Sets container as Central Widget on screen, main focus
-        self.setCentralWidget(container)
+        # Sets tabs as Central Widget on screen, main focus
+        self.setCentralWidget(self.tabs)
+
+		# Calls our function below which creates IDE
+        self.addNewTab()
+
 
         # Status bar for bottom of window, this allows for info to user ('File Saved', etc)
         self.status = QStatusBar()
 
         # Sets status bar to window 
         self.setStatusBar(self.status)
+		
+		# Setting bigger and smaller font
+        largefont = self.status.font()
+        smallfont = self.menuBar().font()
+        largefont.setPointSize(14)
+        smallfont.setPointSize(12)
+        
+        # Applying to elements
+        self.status.setFont(largefont)
+        self.menuBar().setFont(smallfont)
 
         # Creates horizontal bar on top to hold clickable items. Not displayed until below
         file_toolbar = QToolBar("File")
@@ -64,13 +74,26 @@ class MainWindow(QMainWindow):
         open_file_action.setStatusTip("Open file")
 
         # adding action to the open file -- uses function from below 
-        #open_file_action.triggered.connect(self.file_open)
+        open_file_action.triggered.connect(self.openFile)
 
         # This adds to the file dropdown menu 
         file_menu.addAction(open_file_action)
 
         # This adds the Open file option to the toolbar.
         file_toolbar.addAction(open_file_action)
+        
+        # Repeat above for each element. Only Specific elements will be on toolbar
+        save_file_action = QAction ("Save File", self)
+        save_file_action.setStatusTip("Save your hard work so you don't lose it")
+        save_file_action.triggered.connect(self.saveFile)
+        file_menu.addAction(save_file_action)
+        file_toolbar.addAction(save_file_action)
+        
+        new_action = QAction ("New Document", self)
+        new_action.setStatusTip("Start fresh or add on with a new file!")
+        #new_action.triggered.connect(self.saveFileAs)
+        file_menu.addAction(new_action)
+        file_toolbar.addAction(new_action)
         
 #-----------------------Editing features needed for a good text editor, copy and paste, undo, etc-----------------------
         # Edit menu same as above
@@ -83,44 +106,50 @@ class MainWindow(QMainWindow):
         # Actions for toolbar when user clicks these
         undo_action = QAction("Undo", self)             # Undo
         undo_action.setStatusTip("Undo last change")
-        undo_action.triggered.connect(self.editor.undo)
-
+        
+       # Lambda! Lambda is a temporary unnamed function that runs at time of creating. ------->     This is because by calling this without lambda, it will run on					
+        undo_action.triggered.connect(lambda: self.getCurrentEditor().undo())				#    opening of the program. A Lambda function will only run when triggered
+																							# We create a lambda and initialize it to the function below which grabs the 
+																							# editor object for the active tab.				
         # This will add the undo to toolbar created above
         edit_toolbar.addAction(undo_action)
-        edit_menu.addAction(undo_action)
+        edit_menu.addAction(undo_action)					
+								
+										
+       # The lambda defers execution until the user clicks the Undo action.
+										
 
         # Redo button - same as above
         redo_action = QAction("Redo", self)
         redo_action.setStatusTip("Redo last change")
-        redo_action.triggered.connect(self.editor.redo)
+        redo_action.triggered.connect(lambda: self.getCurrentEditor().redo())
         edit_toolbar.addAction(redo_action)
         edit_menu.addAction(redo_action)
 
         # Cut for cut and paste, no toolbar for this
         cut_action = QAction("Cut", self)
         cut_action.setStatusTip("Cut selected text")
-        cut_action.triggered.connect(self.editor.cut)
-        
+        cut_action.triggered.connect(lambda: self.getCurrentEditor().cut())
         # Adding only to the drop down menu
         edit_menu.addAction(cut_action)
 
-        # Copy same as cut
+        # lambda use here allows for very modular function
         copy_action = QAction("Copy", self)
         copy_action.setStatusTip("Copy selected text")
-        copy_action.triggered.connect(self.editor.copy)
+        copy_action.triggered.connect(lambda: self.getCurrentEditor().copy())
         edit_menu.addAction(copy_action)
 
         # Paste will be on toolbar for easy pasting 
         paste_action = QAction("Paste", self)
         paste_action.setStatusTip("Paste from clipboard")           #Refine this, better font and design
-        paste_action.triggered.connect(self.editor.paste)
+        paste_action.triggered.connect(lambda: self.getCurrentEditor().paste())
         edit_toolbar.addAction(paste_action)
         edit_menu.addAction(paste_action)
 
         # Nice little select all text for edit menu. No toolbar as it's only used for certain cases
         select_action = QAction("Select all", self)
         select_action.setStatusTip("Select all text")
-        select_action.triggered.connect(self.editor.selectAll)
+        select_action.triggered.connect(lambda: self.getCurrentEditor().selectAll())
         edit_menu.addAction(select_action)
         
 #----------------------------------------Build Menu such as Compiler and other Dev tools--------------------------------        
@@ -138,6 +167,7 @@ class MainWindow(QMainWindow):
         
         compile_run_action = QAction("Compile and Run", self)
         compile_run_action.setStatusTip("Compile and Run your Program")
+        compile_run_action.triggered.connect(self.compileSetup)
         build_toolbar.addAction(compile_run_action)
         build_menu.addAction(compile_run_action)
         
@@ -149,8 +179,10 @@ class MainWindow(QMainWindow):
 #----------------------------------------Print Menu for the option printing---------------------------------------------        
         print_menu = self.menuBar().addMenu("Print")
         
-        
-  
+        print_action = QAction("Print Document", self)
+        print_action.setStatusTip("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Don't forget to frame it!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+        print_action.triggered.connect(self.filePrint)
+        print_menu.addAction(print_action)
   
 #-------------------------------------------------Settings Menu-----------------------------------------------------------        
         settings_menu = self.menuBar().addMenu("Settings")
@@ -161,29 +193,168 @@ class MainWindow(QMainWindow):
         
         
         
-        
-#----------------------------------End of Menu options and Tool bar------------------------------------------------------
-#---------------------------Below are functions and general QT setup for building the look------------------------------------------------------        
+    
+#------------------------------------------End of Menu/Toolbar --------------------------------------------------------------------------------------    
         # Displaying messages to status bar. This is the welcome message until replaced .
         self.status.showMessage("~~~~~~~~~~~~~~Welcome to The MultiLang IDE --- Share with Friends and Code Away!~~~~~~~~~~~~~~")
         #setStatusTip()
         
         # Redundant show window call 
-        self.show()
+        self.show()    
+    
+        
+        
+        # setStyleSheet() method included with PyQT. Using my experience with CSS it actually was really simple to write. This is just starting code.
+        # https://doc.qt.io/archives/qt-5.15/stylesheet-reference.html
+        self.setStyleSheet("""
+        /* Whole page*/
+    QMainWindow {
+        background-color: #2e2e2e;
+    }
+
+	/* Main text editor */
+    QPlainTextEdit {
+        background-color: #2b3e50;
+        color: #e0e0e0;								/* This is the color of the text in the editor */	
+        font-family: Consolas, monospace;
+        font-size: 14px;							/* Font size and font type*/
+        border: 1px solid #333;						/* Border size and type. Can be dotted, dashed, etc */
+        padding: 6px;								/* Inner spacing */
+    }
+
+	/* File path label*/
+    QLabel {
+        color: #cccccc;
+        font-size: 13px;
+    }
+
+	/* Tab container */
+    QTabWidget::pane {
+        border: 1px solid #444;
+    }
+
+	/* Individual tab styles */
+    QTabBar::tab {
+        background: #2a2a2a;
+        color: #aaa;
+        padding: 6px;
+        border: 1px solid #444;
+        border-bottom: none;
+    }
+	/* Selecting a specific tab */
+    QTabBar::tab:selected {
+        background: #3a3a3a;
+        color: #fff;
+    }
+
+    QStatusBar {
+        background-color: #111;
+        border-top: 1px solid #444;
+    }
+
+    QMenuBar {
+        background-color: #2b2b2b;				/* Dark gray menu bar*/
+        color: #ddd;							/* White text for menu items*/
+    }
+
+    QMenuBar::item:selected {
+        background-color: #444;					/* When an item is hovered background changes */
+    }
+
+    QToolBar {
+        background-color: #2b2b2b;				
+        border-bottom: 1px solid #444;
+    }
+
+    QToolButton {
+        color: #ddd;
+        background-color: #2b2b2b;
+        border: none;
+        padding: 4px 8px;
+    }
+
+    QToolButton:hover {						/* Same hover change for the toolbar */
+        background-color: #444;
+    }
+""")
+        
+        
+        
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#-----------------------------------------Below are functions and general QT setup for building the look-------------------------------------------------------------    
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+###
+
+    def addNewTab(self, file_path=None):
+		
+		# Building editor object using QPlainTextEdit(). Adding line wrapping 
+        editor = QPlainTextEdit()
+        editor.setLineWrapMode(QPlainTextEdit.WidgetWidth)
+
+        # Label to show selected file path
+        label = QLabel(file_path if file_path else "No file selected")
+        #label.setFont(QFont("Arial", 12))
+
+        # Console below main editor to capture/display output 
+        # Creates a new text editor same as above but..
+        output_console = QPlainTextEdit()
+        
+        # Read only overrides the text edit so it only displays added text
+        output_console.setReadOnly(True)
+        output_console.setStyleSheet("background-color: #111; color: #0f0; font-family: monospace;")
+
+		# Vertical layout to stack label, editor, and console 
+        layout = QVBoxLayout()
+        layout.addWidget(label)
+        layout.addWidget(editor)
+        layout.addWidget(output_console)
+        
+        # Container widget that holds everything
+        container = QWidget()
+        container.setLayout(layout)
+
+		# Setting tab name based on file name or default
+							# Basename extracts just the file name "/path/to/test.py" -> "test.py" which is what we want to display
+        tab_name = os.path.basename(file_path) if file_path else "New File"
+        
+        # Adds this container as a new tab with this name 
+        self.tabs.addTab(container, tab_name)
+
+#-------------------------------------------------------------------------------------------------------------------------
+
+       # Helper to get the current editor from the active tab
+    def getCurrentEditor(self):
+        current_widget = self.tabs.currentWidget()
+        return current_widget.findChild(QPlainTextEdit)
+
+
+###
 
         # Override Key Press event for allowing button presses such as ESC to exit
     def keyPressEvent(self, event):
         if event.key() == Qt.Key_Escape:
             self.confirmExit()
+        elif event.key() == Qt.Key_F5:
+            self.compileSetup()
+        elif event.key() == Qt.Key_F1:
+            self.addNewTab()
+
+
+###
 
     def confirmExit(self):
         # Creating a Yes/No message box for the user to confirm their exit. 
         reply = QMessageBox.question(self, "Exit", "Are you sure you want to quit?",
                                      QMessageBox.Yes | QMessageBox.No)
-                                     
+                                                                          
         # If yes, quit application
         if reply == QMessageBox.Yes:
             QApplication.instance().quit()
+
+
+
+###
 
     def openFile(self):
         options = QFileDialog.Options()
@@ -193,7 +364,14 @@ class MainWindow(QMainWindow):
                                                    "All Files (*);;Text Files (*.txt);;Python Files(*.py);;C++ Files(*.cpp, *.h);;JS Files (*.node)", 
                                                    options=options)			# This controls the dropdown in file manager itself
         if file_path:
+            self.path = file_path
+            
+            with open(file_path, 'r') as f:
+                self.editor.setPlainText(f.read())
             self.label.setText(f"Selected File: {file_path}")
+
+
+###
 
     def saveFile(self):
         
@@ -203,25 +381,36 @@ class MainWindow(QMainWindow):
             return self.saveFileAs()
                                 # Otherwise it saves to the path already set
         self.saveToPath()
+
+
+
+
         
     def saveFileAs(self):
         options = QFileDialog.Options()
         file_path, _ = QFileDialog.getSaveFileName(self, "Save File", "", 			# Remove after txt to have default
                                                    "All Files (*);;Text Files (*.txt);;Python Files(*.py);;C++ Files(*.cpp, *.h);;JS Files (*.node)", 
                                                    options=options)
-        if not path:
+        if not self.path:
             return 
 
 	
+
+###
     
-    def compileSetup(self, file):
+    def compileSetup(self):
     # Function from stack to understand the compiler call to other languages @AKX & @mousetail.
     # Modified by me to fit my uses
     
-	# Splits filename into two parts focusing on ext. 1 grabs 2nd part of split (.ext) and lowercases it as it should be
-        ext = os.path.splitext(file)[1].lower()
-        if ext == ".py":
-           return [(sys.executable, file)]		# Built in py command to execute .py files using built in interpreter
+    # If file is not open produces error. This is filled in the openFile function
+        if not self.path:
+          QMessageBox.warning(self, "No File", "Please open a file first.")
+          return
+    
+	# Splits current file path into two parts focusing on .ext. 1 grabs 2nd part of split (.ext) and lowercases it as it should be. 
+        ext = os.path.splitext(self.path)[1].lower()
+        if ext == ".py":							
+           return [(sys.executable, self.path)]		# Built in py command to execute .py files using built in interpreter. Uses path var from above
 		# If its a CPP extension: 
         elif ext in (".c", ".cpp", ".h"):
 			 # Tries to find the gcc compiler path
@@ -249,8 +438,13 @@ class MainWindow(QMainWindow):
         else: 
             raise ValueError(f"Unsupported file type: {ext}")
 
+
+
+
+###
+
     # For Print button if desired.
-    def file_print(self):
+    def filePrint(self):
 
         # Creating a QPrintDialog
         export = QPrintDialog()
@@ -261,6 +455,10 @@ class MainWindow(QMainWindow):
             # Print the text
             self.editor.print_(export.printer())
 
+###
+
+
+    
 
 #--------------------------------------------------------------------------------------------------------------------
 # Standard PyQT setup 
